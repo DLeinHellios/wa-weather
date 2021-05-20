ece19c4797d076003a18e1f3b408b6b75615b483
Dial <Dylan@dleinhellios.com>
2021-05-20 18:34:19 -0500

    Move credential info to .env

--- app.py
+++ app.py
@@ -1,31 +1,45 @@
-import json, os
+import json, os, ast
 from flask import Flask, render_template, redirect, url_for
 from google.oauth2 import service_account
+from dotenv import load_dotenv
 import gspread
 
 
+def get_credential():
+    '''Reads environment vars, builds and returns scoped Google credential object'''
+
+    # Build credential dict
+    account = {
+        "type": "service_account",
+        "project_id": os.environ['GOOGLE_PROJECT_ID'],
+        "private_key_id": os.environ['GOOGLE_PRIVATE_KEY_ID'],
+        "private_key": os.environ['GOOGLE_PRIVATE_KEY'],
+        "client_email": os.environ['GOOGLE_CLIENT_EMAIL'],
+        "client_id": os.environ['GOOGLE_CLIENT_ID'],
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_x509_cert_url": os.environ['GOOGLE_CLIENT_CERT_URL']}
+
+    # Build scopes list
+    scopes = [
+        "https://spreadsheets.google.com/feeds",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"]
+
+    # Construct credentials
+    credential = service_account.Credentials.from_service_account_info(account)
+    scoped_credential = credential.with_scopes(scopes)
+
+    return scoped_credential
+
+
+# Setup
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-
-
-# Download Google service account credentials, rename to 'key.json' for this to work
-#   TODO - move to .env, build dict in code
-
-
-with open('key.json', 'r') as json_creds:
-    acct_info = json.load(json_creds)
-
-
-credential = service_account.Credentials.from_service_account_info(acct_info)
-scopes = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"]
-
-scoped_credential = credential.with_scopes(scopes)
-
-client = gspread.authorize(scoped_credential)
+load_dotenv('.env')
+client = gspread.authorize(get_credential())
 sheet = client.open("weather").sheet1
 
 
